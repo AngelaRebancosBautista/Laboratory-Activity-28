1c08735c4da8bc01f2249a3ee4bddc6a1c41ebcf
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-07-25 12:53:28 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_Activity_28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word: ");
+            string word = Console.ReadLine();
+
+            var reversed = new StringBuilder(word.Length);
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed.Append(word[i]);
+            }
+
+            Console.WriteLine("Reversed: " + reversed);
+        }
+    }
+}
+
+        
+
+
+
+
